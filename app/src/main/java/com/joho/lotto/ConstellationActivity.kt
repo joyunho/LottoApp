1d62d092f99f63134d976d8157ad5b90739f8d22
joyunho <whdbsgh2000@gmail.com>
2021-01-05 19:28:32 +0900

    connect main page

--- app/src/main/java/com/joho/lotto/ConstellationActivity.kt
+++ app/src/main/java/com/joho/lotto/ConstellationActivity.kt
@@ -1,14 +1,25 @@
 package com.joho.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_constellation.*
 
 class ConstellationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
-        Toast.makeText(applicationContext, "ConstellationActivity 입니다", Toast.LENGTH_LONG).show()
+
+        // 로또 번호 확인 버튼의 클릭이벤트 리스너 설정
+        goResultButton.setOnClickListener{
+            // ResultActivity를 시작하는 인텐트 만들고 startActivity 로 실행
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
     }
 }
+
+
+
+
--- app/src/main/java/com/joho/lotto/NameActivity.kt
+++ app/src/main/java/com/joho/lotto/NameActivity.kt
@@ -1,14 +1,29 @@
 package com.joho.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_name.*
 
 class NameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
-        Toast.makeText(applicationContext, "NameActivity 입니다", Toast.LENGTH_LONG).show()
+
+        // 번호 생성 버튼의 클릭이벤트 리스너 설정
+        goButton.setOnClickListener{
+            // ResultActivity 를 시작하는 인텐트를 만들고 startActivity 로 실행
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
+
+        // 뒤로가기 버튼의 클릭이벤트 리스너 설정
+        backButton.setOnClickListener({
+            // 액티비티 종료
+            finish()
+        })
     }
+
+
 }
