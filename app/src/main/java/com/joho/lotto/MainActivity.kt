72ad4f438cd617c4deabf6966aa01ce0b5e7debb
joyunho <whdbsgh2000@gmail.com>
2021-01-06 00:29:18 +0900

    connect main page(Error Update)

--- app/src/main/java/com/joho/lotto/MainActivity.kt
+++ app/src/main/java/com/joho/lotto/MainActivity.kt
@@ -3,8 +3,6 @@ package com.joho.lotto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         // 별자리로 번호 생성 카드의 클릭 이벤트 리스너
-        constellationCard.setOnClickListener{
+        constellationCard.setOnClickListener {
             // ConstellationActivity 를 시작하는 Intent 를 만들고 startActivity 로 실행
             startActivity(Intent(this, ConstellationActivity::class.java))
         }
